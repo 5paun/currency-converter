f1968f3485adbd75fff145b1873e039ae54dc84e
Denis <dzianis.shalashou@innowise-group.com>
2021-09-22 17:40:23 +0300

    defect/removed-conosle-log

--- src/redux/sagas/currency/index.js
+++ src/redux/sagas/currency/index.js
@@ -10,7 +10,6 @@ function * getCurrentIpSagaWorker () {
     yield put({ type: USER_DATA_RESPONSE, payload: { ipAddress: currentIP.data.ipAddress, currencyCodeCurrentLocation: currentPosition.data.currency.code.toLowerCase() } })
     yield call(convertSelectedCurrencySagaWorker)
   } catch (error) {
-    console.log('error', error)
     yield put({ type: USER_DATA_RESPONSE_FAIL, payload: error.message })
   }
 }
@@ -20,7 +19,6 @@ function * getCurrenciesSagaWorker () {
     const response = yield call(ConverterService.getCurrencies)
     yield put({ type: CURRENCIES_RESPONSE, payload: Object.entries(response.data).map(item => ({ value: item[0], label: item[1] })) })
   } catch (error) {
-    console.log('error', error)
     yield put({ type: CURRENCIES_RESPONSE_FAIL, payload: error.message })
   }
 }
@@ -33,7 +31,6 @@ function * convertSelectedCurrencySagaWorker () {
       yield put({ type: SET_LOCAL_CURRENCY_RESPONSE, payload: response.data })
     }
   } catch (error) {
-    console.log('error', error)
     yield put({ type: SET_LOCAL_CURRENCY_RESPONSE_FAIL, payload: error.message })
   }
 }
